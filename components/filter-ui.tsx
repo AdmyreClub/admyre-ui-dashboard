ba6143eeb8540059d43b287a13b7fd15d1f82a6f
Khelan Mehta <107768499+khelan-mehta@users.noreply.github.com>
2024-01-18 11:23:43 +0530

    Update filter-ui.tsx

--- components/filter-ui.tsx
+++ components/filter-ui.tsx
@@ -34,6 +34,7 @@ import LocationFilterUI from "./filters/location-filter";
 import LanguageFilterUI from "./filters/language-filter";
 import CategoriesFilterUI from "./filters/categories-filter";
 import EngagementFilterUI from "./filters/engagement-filter";
+import GenderForm from "./filters/genders-filter";
 
   type FiltersType = z.infer<typeof filtersSchema>;
   type followersRangedSchemaType = z.infer<typeof followersRangedSchema>;
@@ -42,44 +43,44 @@ import EngagementFilterUI from "./filters/engagement-filter";
   const FilterUI = () => {
     const [filters, setFilters] = React.useState(initialFiltersState);
 
+    const [followerData, setFollowerData] = React.useState([0, 100000000])
+    const [genderData, setGenderData] = React.useState("male")
+    const [followingData, setFollowingData] = React.useState([0, 100000000])
+    const [engagementRateData, setEngagementRateData] = React.useState([0, 100])
+
+    const handleFollowerData = (dataFromChild: [number, number]) => {
+      // Do something with the data received from the child component
+      setFollowerData(dataFromChild);
+    };
+    const handleEngagementRateData = (dataFromChild: [number, number]) => {
+      // Do something with the data received from the child component
+      setEngagementRateData(dataFromChild);
+    };
+    const handleGenderData = (dataFromChild: string) => {
+      // Do something with the data received from the child component
+      setGenderData(dataFromChild);
+    };
+    const handleFollowingData = (dataFromChild: [number, number]) => {
+      // Do something with the data received from the child component
+      setFollowingData(dataFromChild);
+    };
+
     const { register, handleSubmit, reset, formState: { errors } } = useForm<FiltersType>({
       resolver: zodResolver(filtersSchema),
       defaultValues: initialFiltersState
     });
 
-    const {register: registerFollowers, handleSubmit: handleSubmitFollowers, control: controlFollowers, reset: resetFollowers, formState: {errors: errorFollowers}} = useForm<followersRangedSchemaType>({
-      resolver: zodResolver(followersRangedSchema),
-      defaultValues: {
-        followers: { from: null, to: null },
-      },
-    });
-
-    const {register: registerFollowings, handleSubmit: handleSubmitFollowings, control: controlFollowings, reset: resetFollowings, formState: {errors: errorFollowings}} = useForm<followingsRangedSchemaType>({
-      resolver: zodResolver(followingsRangedSchema),
-      defaultValues: {
-        followings: { from: null, to: null },
-      },
-    });
-
     const onSubmitMain = (data: FiltersType) => {
       console.log("Main data: ",data);
+      console.log("follower data: " , followerData);
+      console.log("engagementRate data: " , engagementRateData);
+      console.log("gender data: " , genderData);
+      console.log("following data: " , followingData);
     };
     const handleResetMain = () => {
       reset();
     };
-    const onSubmitFollowers = (data: followersRangedSchemaType) => {
-      console.log("Follower data: ",data);
-    }
-    const handleResetFollowers = () => {
-      resetFollowers();
-    };
 
-    const onSubmitFollowings = (data: followingsRangedSchemaType) => {
-      console.log("Followings data: ",data);
-    }
-    const handleResetFollowings = () => {
-      resetFollowings();
-    };
 
     return (
       <Card className="w-full mb-8">
@@ -106,7 +107,7 @@ import EngagementFilterUI from "./filters/engagement-filter";
                         <Button variant="outline" className="flex space-x-2">Followers <ChevronDown /></Button>
                       </PopoverTrigger>
                       <PopoverContent className="w-80">
-                        <FollowerFilter />
+                        <FollowerFilter onDataFromChild={handleFollowerData} defaultVal={followerData}/>
                       </PopoverContent>
                   </Popover>
                 </div>
@@ -116,7 +117,7 @@ import EngagementFilterUI from "./filters/engagement-filter";
                         <Button variant="outline" className="flex space-x-2">Followings <ChevronDown /> </Button>
                       </PopoverTrigger>
                       <PopoverContent className="w-80">
-                        <FollowingsFilterUI />
+                        <FollowingsFilterUI onDataFromChild={handleFollowingData} defaultVal={followingData}/>
                       </PopoverContent>
                     </Popover>
                   </div>
@@ -229,38 +230,7 @@ import EngagementFilterUI from "./filters/engagement-filter";
                         <Button variant="outline" className="flex space-x-2">Engagement Rate <ChevronDown /> </Button>
                       </PopoverTrigger>
                       <PopoverContent className="w-80">
-                        <div className="grid gap-4">
-                          <div className="space-y-2">
-                            <h4 className="font-medium leading-none">Engagement Rate</h4>
-                            <p className="text-sm text-muted-foreground">
-                              Set the interval for engagement rates.
-                            </p>
-                          </div>
-                          <div className="grid gap-5">
-                            <div className="grid grid-cols-3 items-center gap-4">
-                              <Label htmlFor="width">Minimum </Label>
-                              <Input
-                                id="width"
-                                defaultValue=""
-                                placeholder="minimum value (0)"
-                                className="col-span-2 h-8"
-                              />
-                            </div>
-                            <div className="grid grid-cols-3 items-center gap-4">
-                              <Label htmlFor="maxWidth">Maximum</Label>
-                              <Input
-                                id="maxWidth"
-                                defaultValue=""
-                                placeholder="maximum value (?)"
-                                className="col-span-2 h-8"
-                              />
-                            </div>
-                          </div>
-                        </div>
-                        <div id="CardFooter" className="flex justify-between mt-5">
-                          <Button variant="outline">Clear</Button>
-                          <Button>Apply</Button>
-                        </div>
+                        <EngagementFilterUI onDataFromChild={handleEngagementRateData} defaultVal={engagementRateData}/>
                       </PopoverContent>
                     </Popover>
                   </div>
@@ -270,48 +240,7 @@ import EngagementFilterUI from "./filters/engagement-filter";
                         <Button variant="outline" className="flex space-x-2">Gender <ChevronDown /> </Button>
                       </PopoverTrigger>
                       <PopoverContent className="w-80">
-                        <div className="grid gap-4">
-                          <div className="space-y-2">
-                            <h4 className="font-medium leading-none">Dimensions</h4>
-                            <p className="text-sm text-muted-foreground">
-                              Set the dimensions for the layer.
-                            </p>
-                          </div>
-                          <div className="grid gap-2">
-                            <div className="grid grid-cols-3 items-center gap-4">
-                              <Label htmlFor="width">Width</Label>
-                              <Input
-                                id="width"
-                                defaultValue="100%"
-                                className="col-span-2 h-8"
-                              />
-                            </div>
-                            <div className="grid grid-cols-3 items-center gap-4">
-                              <Label htmlFor="maxWidth">Max. width</Label>
-                              <Input
-                                id="maxWidth"
-                                defaultValue="300px"
-                                className="col-span-2 h-8"
-                              />
-                            </div>
-                            <div className="grid grid-cols-3 items-center gap-4">
-                              <Label htmlFor="height">Height</Label>
-                              <Input
-                                id="height"
-                                defaultValue="25px"
-                                className="col-span-2 h-8"
-                              />
-                            </div>
-                            <div className="grid grid-cols-3 items-center gap-4">
-                              <Label htmlFor="maxHeight">Max. height</Label>
-                              <Input
-                                id="maxHeight"
-                                defaultValue="none"
-                                className="col-span-2 h-8"
-                              />
-                            </div>
-                          </div>
-                        </div>
+                        <GenderForm onDataFromChild={handleGenderData} defaultVal={genderData}/>
                       </PopoverContent>
                     </Popover>
                   </div>
