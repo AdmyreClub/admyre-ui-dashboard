c6b52ea764f9ce5657bc4556585c0db1fc555a6d
Khelan Mehta <107768499+khelan-mehta@users.noreply.github.com>
2024-01-18 11:21:55 +0530

    Update followings-filter.tsx

--- components/filters/followings-filter.tsx
+++ components/filters/followings-filter.tsx
@@ -3,97 +3,175 @@ import * as React from "react";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
-import { Slider } from "@/components/ui/slider";
-import { ChevronDown } from "lucide-react";
+import { RangeSlider } from "@/components/ui/range-slider";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 
-
-const followingSchema = z.object({
+const validationSchema = z.object({
   min: z
-    .string()
-    .transform((val) => (val === "" ? null : Number(val)))
-    .refine((val) => val === null || !isNaN(val), {
+    .number()
+    .nullable()
+    .refine((val) => val == null || !isNaN(val), {
       message: "Minimum must be a number",
-    })
-    .nullable(),
+    }),
   max: z
-    .string()
-    .transform((val) => (val === "" ? null : Number(val)))
-    .refine((val) => val === null || !isNaN(val), {
+    .number()
+    .nullable()
+    .refine((val) => val == null || !isNaN(val), {
       message: "Maximum must be a number",
-    })
-    .nullable(),
+    }),
 });
 
-type followingSchema = z.infer<typeof followingSchema>;
+const MAX_SLIDER_COUNT = 10000000;
 
-const FollowingsFilterUI = () => {
+interface ChildProps {
+  onDataFromChild: (data: [number, number]) => void;
+  defaultVal: [number, number];
+}
+
+type ValidationSchema = z.infer<typeof validationSchema>;
+
+const FollowingFilter = ({ onDataFromChild, defaultVal }: ChildProps) => {
   const {
     register,
     handleSubmit,
     reset,
     formState: { errors },
-  } = useForm<followingSchema>({
-    resolver: zodResolver(followingSchema),
+    setValue,
+    watch,
+    control,
+  } = useForm<ValidationSchema>({
+    resolver: zodResolver(validationSchema),
+    defaultValues: {
+      min: defaultVal[0], // Provide a default value for min
+      max: defaultVal[1], // Provide a default value for max
+    },
   });
+  const { min, max } = watch();
 
-  const onSubmit: SubmitHandler<followingSchema> = (data) => console.log(data);
+  const sendDataToParent = () => {
+    // Send data to the parent component using the callback function
+    onDataFromChild([min, max]);
+  };
+
+  function linearToInfluencerScale(value: number, max: number) {
+    let scaledValue;
+    if (value <= max * 0.25) {
+      // Nano-Influencers
+      scaledValue = 1000 + (value / (max * 0.25)) * (25000 - 1000);
+    } else if (value <= max * 0.5) {
+      // Micro-Influencers
+      scaledValue =
+        25000 + ((value - max * 0.25) / (max * 0.25)) * (100000 - 25000);
+    } else if (value <= max * 0.75) {
+      // Macro-Influencers
+      scaledValue =
+        100000 + ((value - max * 0.5) / (max * 0.25)) * (1000000 - 100000);
+    } else {
+      // Celebrities
+      scaledValue =
+        1000000 +
+        ((value - max * 0.75) / (max * 0.25)) * (MAX_SLIDER_COUNT - 1000000);
+    }
+    return Math.round(scaledValue);
+  }
+
+  function influencerScaleToLinear(value: number, max: number) {
+    let linearValue;
+    if (value <= 25000) {
+      // Nano-Influencers
+      linearValue = ((value - 1000) / (25000 - 1000)) * (max * 0.25);
+    } else if (value <= 100000) {
+      // Micro-Influencers
+      linearValue =
+        max * 0.25 + ((value - 25000) / (100000 - 25000)) * (max * 0.25);
+    } else if (value <= 1000000) {
+      // Macro-Influencers
+      linearValue =
+        max * 0.5 + ((value - 100000) / (1000000 - 100000)) * (max * 0.25);
+    } else {
+      // Celebrities
+      linearValue =
+        max * 0.75 +
+        ((value - 1000000) / (MAX_SLIDER_COUNT - 1000000)) * (max * 0.25);
+    }
+    return Math.round(linearValue);
+  }
+
+  const onSliderChange = (values: [number, number]) => {
+    const scaledMin = linearToInfluencerScale(values[0], MAX_SLIDER_COUNT);
+    const scaledMax = linearToInfluencerScale(values[1], MAX_SLIDER_COUNT);
+
+    setValue("min", scaledMin);
+    setValue("max", scaledMax);
+  };
+
+  const parseNumber = (value: string) => {
+    const parsed = parseInt(value, 10);
+    return isNaN(parsed) ? null : parsed;
+  };
+  const onSubmit: SubmitHandler<ValidationSchema> = (data) => console.log(data);
+
   return (
-    <>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="grid gap-4">
-          <div className="space-y-2">
-            <h4 className="font-medium leading-none">Followings Range</h4>
-            <p className="text-sm text-muted-foreground">
-              Set the interval for following counts.
-            </p>
-          </div>
-          <div className="grid gap-5">
-            <div className="grid grid-cols-3 items-center gap-4">
-              <Label htmlFor="width">Minimum </Label>
-              <Input
-                id="width"
-                defaultValue=""
-                placeholder="minimum value (0)"
-                className="col-span-2 h-8"
-                {...register("min")}
-              />
-            </div>
-            <div className="grid grid-cols-3 items-center gap-4">
-              <Label htmlFor="maxWidth">Maximum</Label>
-              <Input
-                id="maxWidth"
-                defaultValue=""
-                placeholder="maximum value (?)"
-                className="col-span-2 h-8"
-                {...register("max")}
-              />
-            </div>
-            <Slider defaultValue={[33]} max={100} step={1} />
-          </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="grid gap-4">
+        <h4 className="font-medium leading-none">Following Range</h4>
+        <p className="text-sm text-muted-foreground">
+          Set the interval for Following counts.
+        </p>
+
+        <div className="grid grid-cols-3 items-center gap-4">
+          <Label htmlFor="minFollowings">Minimum</Label>
+          <Input
+            id="minFollowings"
+            placeholder="Minimum value (0)"
+            className="col-span-2 h-8"
+            {...register("min", { setValueAs: parseNumber })}
+          />
         </div>
-        <div id="CardFooter" className="flex justify-between mt-5">
-          <Button variant="outline" onClick={() => reset()}>Clear</Button>
-          <Button>Apply</Button>
+
+        <div className="grid grid-cols-3 items-center gap-4">
+          <Label htmlFor="maxFollowings">Maximum</Label>
+          <Input
+            id="maxFollowings"
+            placeholder="Maximum value"
+            className="col-span-2 h-8"
+            {...register("max", { setValueAs: parseNumber })}
+          />
         </div>
-      </form>
-    </>
+
+        <RangeSlider
+          value={[
+            influencerScaleToLinear(min || 0, MAX_SLIDER_COUNT),
+            influencerScaleToLinear(max || MAX_SLIDER_COUNT, MAX_SLIDER_COUNT),
+          ]}
+          onValueChange={onSliderChange}
+          min={0}
+          max={MAX_SLIDER_COUNT}
+        />
+
+        {errors.min && <p className="text-red-500">{errors.min.message}</p>}
+        {errors.max && <p className="text-red-500">{errors.max.message}</p>}
+
+        <div className="flex justify-between mt-5">
+          <Button
+            variant="outline"
+            onClick={() => {
+              setValue("min", 0);
+              setValue("max", 100000000);
+              onDataFromChild([0,100000000])
+            }}
+          >
+            Clear
+          </Button>
+          <Button type="submit" onClick={sendDataToParent}>
+            Apply
+          </Button>
+        </div>
+      </div>
+    </form>
   );
 };
 
-export default FollowingsFilterUI;
+export default FollowingFilter;
