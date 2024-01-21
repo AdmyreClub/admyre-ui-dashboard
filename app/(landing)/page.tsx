a6c647bf92ace6f5251b413fcc821e6f755a1319
Khelan Mehta <107768499+khelan-mehta@users.noreply.github.com>
2024-01-22 00:29:52 +0530

    Update page.tsx

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,19 +1,10 @@
-import { Button } from '@/components/ui/button'
-import Image from 'next/image'
-import Link from 'next/link'
+import { redirect } from 'next/navigation';
+function isLoggedIn() {
 
-export default function Home() {
-  return (
-    <div className=''>
-      Landing page (unprotected)
-      <div>
-        <Link href="/login">
-          <Button>Login</Button>
-        </Link>
-        <Link href="/register">
-          <Button>Register</Button>
-        </Link>
-      </div>
-    </div>
-  )
+}
+export default async function Home() {
+    console.log(isLoggedIn());
+  
+    redirect('/login');
+  // ...
 }
