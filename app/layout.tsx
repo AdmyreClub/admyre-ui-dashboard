b6e944707a8c90721734e08d6829b5c22929b39e
Khelan Mehta <107768499+khelan-mehta@users.noreply.github.com>
2024-01-22 00:30:08 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,41 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import { ClerkProvider } from '@clerk/nextjs'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
-
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Influencer Search',
-  description: 'Data driven marketing for the modern world',
-}
+  title: "Influencer Search",
+  description: "Data driven marketing for the modern world",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          footerActionLink: {
+            backgroundColor: "white",
+            color: "black",
+          },
+          formButtonPrimary: "bg-black hover:bg-black text-sm normal-case",
+          card:"shadow-none "
+          
+
+        },
+        variables: {
+          colorText: "black",
+        },
+      }}
+    >
       <html lang="en">
         <body className={inter.className}>{children}</body>
       </html>
     </ClerkProvider>
-  )
+  );
 }
