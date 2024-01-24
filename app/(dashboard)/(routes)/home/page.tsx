ca8ea78728ce96f9d20f63bfc3c66eb0185d35a1
colossalhitchhiker <donquixotesoup@gmail.com>
2024-01-25 00:07:08 +0530

    ajkshjks

--- app/(dashboard)/(routes)/home/page.tsx
+++ app/(dashboard)/(routes)/home/page.tsx
@@ -5,7 +5,7 @@ export default function DashboardPage() {
       <div>
         <div className="mb-8 space-y-4 pt-[4.5rem]">
           <h2 className="text-2xl md:text-4xl font-bold ">
-          Instantly Connect with Influencers from the Largest Database
+          Instantly Connect with Influencers from the Largest sdkfjhsjfksldskdjflksdjfs
           </h2>
           <p>Welcome back, {}</p>
         </div>
